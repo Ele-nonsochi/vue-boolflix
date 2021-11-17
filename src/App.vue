42f2c0694f06d8878cfc1eb35f3e3062928131db
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-17 18:36:02 +0100

    Inizio lavoro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
